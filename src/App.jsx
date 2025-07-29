25bb5c9eb521d9a1b9862501402cb72849667986
Zeeshan412 <101563589+Zeeshan412@users.noreply.github.com>
2025-07-29 16:20:34 +0500

    the import line in app.jsx fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import About from './components/Aboutus.jsx'
 import Footer from './components/Footer.jsx'
